f2c859fea94af7ffc798bc91b356bf3961c902fb
Thomas Li Fredriksen <thomas.fredriksen@oceandata.earth>
2022-08-16 15:39:41 +0200

    Ported fom flask to tornado

--- {{cookiecutter.project_slug}}/odp/app/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/odp/app/{{cookiecutter.project_slug}}/__main__.py
@@ -1,33 +1,50 @@
-from flask import Flask
+import asyncio
+
+import tornado.web
+import dotenv
 import click
 
-from typing import *
+
+class MainHandler(tornado.web.RequestHandler):
+    """Main request handler"""
+
+    def get(self) -> None:
+        """GET request handler. Returns with "hello world"-like string"""
+        self.write("<p>Hello, tornado</p>")
 
 
-app = Flask("Hello world")
+async def run_server(
+    host: str,
+    port: int,
+    debug: bool,
+) -> None:
+    app = tornado.web.Application([(r"/", MainHandler)], debug=debug)
 
-@app.route("/")
-def root_endpoint() -> str:
-    return "<p>Hello, World</p>"
+    app.listen(port=port, address=host)
+
+    await asyncio.Event().wait()
+
 
 @click.command()
 @click.option("--host", default="0.0.0.0", help="The hostname to listen on")
-@click.option("--port", default=None, help="The port of the webserver")
-@click.option("--debug", is_flag=True, help="Enable debug-mode")
-@click.option("--load-dotenv/--no-load-dotenv", default=True,
-              help="Load the nearest .env and .flaskenv files to set environment variables")
+@click.option("--port", default=8888, help="The port of the webserver")
+@click.option("--debug", is_flag=True, default=False, help="Enable debug-mode")
+@click.option(
+    "--load-dotenv/--no-load-dotenv",
+    default=True,
+    help="Load the nearest .env and .flaskenv files to set environment variables",
+)
 def main(
-        host: Optional[str],
-        port: Optional[int],
-        debug: Optional[bool],
-        load_dotenv: bool,
+    host: str,
+    port: int,
+    debug: bool,
+    load_dotenv: bool,
 ) -> None:
-    app.run(
-        host=host,
-        port=port,
-        debug=debug,
-        load_dotenv=load_dotenv
-    )
+    if load_dotenv:
+        dotenv.load_dotenv()
+
+    asyncio.run(run_server(host, port, debug))
+
 
 if __name__ == "__main__":
     main()
