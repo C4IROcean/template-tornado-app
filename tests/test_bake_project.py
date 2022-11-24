a62ef795602c0f77a6505d23ba77c4163fc889d8
Thomas Li Fredriksen <thomas.fredriksen@oceandata.earth>
2022-11-24 11:33:15 +0100

    Added python version to tests

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -2,10 +2,18 @@
 import os
 import shlex
 import subprocess
+import sys
 from contextlib import contextmanager
 
 from cookiecutter.exceptions import FailedHookException
 from cookiecutter.utils import rmtree
+from pytest import fixture
+
+
+@fixture
+def python_version():
+    v = sys.version_info
+    return f"{v.major}.{v.minor}"
 
 
 @contextmanager
@@ -37,8 +45,9 @@ def run_inside_dir(command, dirpath):
         return subprocess.check_call(shlex.split(command))
 
 
-def test_bake_with_defaults(cookies):
-    with bake_in_temp_dir(cookies) as result:
+def test_bake_with_defaults(cookies, python_version):
+    context = {"python_version": python_version}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert result.project_path.is_dir()
         assert result.exit_code == 0
         assert result.exception is None
@@ -56,8 +65,8 @@ def test_bake_with_defaults(cookies):
         assert ".gitignore" in found_toplevel_files
 
 
-def test_bake_with_custom_name(cookies):
-    context = {"project_name": "app-awesome"}
+def test_bake_with_custom_name(cookies, python_version):
+    context = {"project_name": "app-awesome", "python_version": python_version}
     with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert result.project_path.is_dir()
         assert result.exit_code == 0
@@ -67,8 +76,9 @@ def test_bake_with_custom_name(cookies):
         assert (result.project_path / "app_awesome").exists
 
 
-def test_bake_and_run_precommit_hooks(cookies):
-    with bake_in_temp_dir(cookies) as result:
+def test_bake_and_run_precommit_hooks(cookies, python_version):
+    context = {"python_version": python_version}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert result.project_path.is_dir()
         assert run_inside_dir("git init", str(result.project_path)) == 0
         assert run_inside_dir("git add .", str(result.project_path)) == 0
@@ -76,21 +86,22 @@ def test_bake_and_run_precommit_hooks(cookies):
         print("test_bake_and_run_precommit_hooks path", str(result.project_path))
 
 
-def test_bake_and_run_tests(cookies):
-    with bake_in_temp_dir(cookies) as result:
+def test_bake_and_run_tests(cookies, python_version):
+    context = {"python_version": python_version}
+    with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert result.project_path.is_dir()
         assert run_inside_dir("poetry install", str(result.project_path)) == 0
         assert run_inside_dir("poetry run pytest tests", str(result.project_path)) == 0
         print("test_bake_and_run_tests path", str(result.project_path))
 
 
-def test_bake_with_prefix_name_fail(cookies):
-    context = {"project_name": "mydata_awesome"}
+def test_bake_with_prefix_name_fail(cookies, python_version):
+    context = {"project_name": "mydata_awesome", "python_version": python_version}
     with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert isinstance(result.exception, FailedHookException)
 
 
-def test_bake_with_hyphen_slug_fail(cookies):
-    context = {"project_slug": "dataproject-awesome"}
+def test_bake_with_hyphen_slug_fail(cookies, python_version):
+    context = {"project_slug": "dataproject-awesome", "python_version": python_version}
     with bake_in_temp_dir(cookies, extra_context=context) as result:
         assert isinstance(result.exception, FailedHookException)
